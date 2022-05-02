423a2a2b80897cc372b447cab79cc3971675e307
nagima Dubanaeva <98920760+nagimad@users.noreply.github.com>
2022-05-02 16:24:57 +0000

    git hub song

--- src/enemy.py
+++ src/enemy.py
@@ -3,6 +3,8 @@ import random
 #model
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, name, x, y, img_file):
+        '''Initializes the enemy
+      args: self, name (str), x(int), y(int), img_file(str) '''
         #initialize all the Sprite functionality
         pygame.sprite.Sprite.__init__(self)
 
@@ -21,4 +23,9 @@ class Enemy(pygame.sprite.Sprite):
         self.speed = 2
 
     def update(self):
-        print("'Update me,' says " + self.name)
+        '''updates the Enemy object
+      args: self'''
+        self.rect.x = self.rect.x + random.choice((-1,0,1))
+        self.rect.y = self.rect.y + random.choice((-1,0,1))
+      
+        #print("'Update me,' says " + self.name)
